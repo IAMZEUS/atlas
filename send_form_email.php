233c96adb6328fd233cd1210f1f2d72a19757e84
Ethan Davidoff <ethand85@gmail.com>
2016-11-10 17:24:00 -0800

    reverting back to old submit form

--- send_form_email.php
+++ send_form_email.php
@@ -1,25 +1,25 @@
- <?php
-if(isset($_POST['submit'])) {
+<?php
+if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "ethan@paywithatlas.com";
-    $email_subject = "Atlas Contact Form Submission";
+    $email_to = "ethand85@gmail.com";
+    $email_subject = "Atlas Form Submission";
  
-
  
     // validation expected data exists
-    if(!isset($_POST['name']) ||        
+    if(!isset($_POST['first_name']) ||        
         !isset($_POST['email'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
-    $name_from = $_POST['name']; // required
+    $first_name = $_POST['first_name']; // required
+  
     $email_from = $_POST['email']; // required    
     $phone_from = $_POST['phone']; // required    
-    // $organization_from = $_POST['organization']; // required    
-    // $role_from = $_POST['role']; // required
-	// $providers_from = $_POST['providers']; // required 
-	// $patients_from = $_POST['patients']; // required     
+    $organization_from = $_POST['organization']; // required    
+    $role_from = $_POST['role'];
+	$providers_from = $_POST['providers']; // required 
+	$patients_from = $_POST['patients']; // required      
     // $comments = $_POST['comments']; // required
  
     $error_message = "";
@@ -28,10 +28,9 @@ if(isset($_POST['submit'])) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
     $string_exp = "/^[A-Za-z\s.'-]+$/";
-  if(!preg_match($string_exp,$name_from)) {
+  if(!preg_match($string_exp,$first_name)) {
     $error_message .= 'The Name you entered does not appear to be valid.<br />';
   }
-
   // if(strlen($comments) < 2) {
   //   $error_message .= 'The Comments you entered do not appear to be valid.<br />';
   // }
@@ -45,11 +44,11 @@ if(isset($_POST['submit'])) {
       return str_replace($bad,"",$string);
     }
  
-    $email_message .= "First Name: ".clean_string($name_from)."\n";
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";   
     $email_message .= "Phone: ".clean_string($phone_from)."\n";
-    $email_message .= "Organization: ".clean_string($organization_from)."\n";
-    $email_message .= "Role: ".clean_string($role_from)."\n";
+    $email_message .= "Organization: ".clean_string($organization)."\n";
+    $email_message .= "Role: ".clean_string($role)."\n";
 	$email_message .= "Providers: ".clean_string($providers_from)."\n";
 	$email_message .= "Patients: ".clean_string($patients_from)."\n";
  
@@ -59,7 +58,9 @@ $headers = 'From: '.$email_from."\r\n".
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 sleep(1);
-
-echo "<script>alert('Thank You. An Atlas representative will contact you soon.')</script> <meta http-equiv='refresh' content=\"0; url=http://paywithatlas.com\">";
+echo "<script>alert('Thank you. An Atlas representative will contact you soon')</script> <meta http-equiv='refresh' content=\"0; url=http://paywithatlas.com\">";
 ?>
  
+<?php
+}
+?>
